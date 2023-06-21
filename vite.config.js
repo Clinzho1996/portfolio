461dcd31aeec3bf99a4a3e5143c44db75e298780
Clinzho1996 <confidinho@yahoo.com>
2023-06-21 15:04:10 +0100

    pwa integrated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      mode: "development",
+      registerType: "autoUpdate",
+      includeAssets: ["./src/assets/logo.png", "./src/assets/*", "./sw.js"],
+    }),
+  ],
+});
